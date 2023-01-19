78accdd15a00cdd3328a9ace8bdc3f9f636eaa33
Douglas Shanks <15614951+douglas-shanks@users.noreply.github.com>
2023-01-19 10:18:57 +0000

    Add VH1-io working example.

--- tests/workshops/VH1/vh1-io.py
+++ tests/workshops/VH1/vh1-io.py
@@ -7,12 +7,17 @@ class BuildRunVH1IOTest(rfm.RegressionTest):
     descr = ('Build and run the VH1 mini-app')
     valid_systems = ['archer2:compute']
     valid_prog_environs = ['PrgEnv-cray','PrgEnv-gnu','PrgEnv-aocc']
-    sourcesdir = 'src/src'
-    executable = './bin/vh1-mpi'
+    modules = ['cray-hdf5','cray-netcdf'] #,'cpe/22.04']
+    #env_vars = {'LD_LIBRARY_PATH': '${CRAY_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}'}
+    env_vars={'NO_STOP_MESSAGE': {'1'}, 'OMP_NUM_THREADS': {'1'}, 'STRIPE_SIZE': {'4m'}, 'STRIPE_COUNT': {'1'} }
+    sourcesdir = 'src'
+    executable = './vh1-mpi'
     build_system = 'Make'
     num_tasks=128
     num_tasks_per_node=128
     num_cpus_per_task=1
+    prerun_cmds = ['lfs setstripe -c ${STRIPE_COUNT} -S ${STRIPE_SIZE} output']
+    postrun_cmds =['cat output/NCState.hst']
     tags = {'production', 'craype'}
 
     @sanity_function
